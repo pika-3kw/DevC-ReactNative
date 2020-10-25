67b15df0a2b2eaf80322c2cf8c4f3c2e52f0c101
Giang Nam <giangnam10a2@gmail.com>
2020-10-25 13:52:13 +0700

    Login screen

--- screens/Login.js
+++ screens/Login.js
@@ -1,13 +1,56 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+
+import { MaterialIcons } from "@expo/vector-icons";
+import { FontAwesome } from "@expo/vector-icons";
+
+import { Button } from "react-native-elements";
+import { Input } from "react-native-elements";
 
 export default Login = (props) => {
   return (
     <View style={styles.screen}>
-      <Text>Login screen</Text>
+      <Text style={styles.title}>Login</Text>
+
+      <View style={styles.inputContainer}>
+        <Input
+          placeholder="Email"
+          leftIcon={<MaterialIcons name="email" size={24} color="black" />}
+        />
+        <Input
+          placeholder="Password"
+          leftIcon={
+            <FontAwesome
+              name="lock"
+              size={24}
+              color="black"
+              style={{ marginLeft: 5, marginRight: 2 }}
+            />
+          }
+          secureTextEntry={true}
+        />
+      </View>
+
+      <View style={styles.buttonContainer}>
+        <View style={{ width: "40%" }}>
+          <Button
+            title="Register"
+            type="outline"
+            onPress={() => props.navigation.navigate("Register")}
+          />
+        </View>
+
+        <View style={{ width: "40%" }}>
+          <Button title="Login" />
+        </View>
+      </View>
+
+      <Text style={{ color: "grey", marginVertical: 20 }}>OR</Text>
+
       <Button
-        title="Go to Register"
-        onPress={() => props.navigation.navigate("Register")}
+        title="Login with Facebook"
+        buttonStyle={{ backgroundColor: "#1877F2" }}
+        containerStyle={{ width: "80%" }}
       />
     </View>
   );
@@ -18,5 +61,16 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+  },
+  inputContainer: {
+    width: "90%",
   },
 });
