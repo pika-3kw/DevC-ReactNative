4da2a695dde2bc98a1e3a7076ae7d82cca48688a
pika-3kw <giangnam10a2@gmail.com>
2020-11-15 23:06:29 +0700

    New Campaign

--- function/fbLogin.js
+++ function/fbLogin.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { BACKEND_URL } from "../constants";
 
 export default async (token) => {
-  console.log(`${BACKEND_URL}` + `/auth/facebook/login/`);
   try {
     const user = await axios.post(`${BACKEND_URL}` + `/auth/facebook/login/`, {
       fbToken: token,
